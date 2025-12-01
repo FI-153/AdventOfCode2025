5bb8d32bf1772b45461a1885f66db43b2348b353
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-12-01 12:08:52 +0100

    Day 1

---
+++ Day1/Problem2/Day1_problem2.py
@@ -0,0 +1,24 @@
+start_pos = 50
+num_elems = 100
+
+safe = [i for i in range(num_elems)]
+index = start_pos
+password = 0
+
+with open('../input.txt', 'r') as file:
+    for line in file:
+
+        amount = int(line[1:])
+        mvmnt = 1 if line[0] == "R" else -1
+
+        for _ in range(0, amount):
+
+            if index == 0 and mvmnt == -1:
+                index = 99
+            else :
+                index = (index + mvmnt) % num_elems
+
+            if index == 0:
+                password+=1
+
+print(password)
---
+++ Day1/Problem_1/Day1_problem1.py
@@ -0,0 +1,16 @@
+start_pos = 50
+num_elems = 100
+
+safe = [i for i in range(num_elems)]
+index = start_pos
+password = 0
+
+with open('../input.txt', 'r') as file:
+    for line in file:
+        amount = int(line[1:]) if line[0]=="R" else -int(line[1:])
+        index = (index + amount)%num_elems
+
+        if index == 0:
+            password+=1
+
+print(password)
