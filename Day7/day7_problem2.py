40131858d6b7cd70d33194ac4f7c3cb10722f4ea
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-12-09 12:20:47 +0100

    Update day7_problem2.py

--- Day7/day7_problem2.py
+++ Day7/day7_problem2.py
@@ -1,38 +1,30 @@
 from collections import defaultdict
 
+# Apply dynamic programming to tackle an otherwise combinatorial problem
 def num_timelines():
     lines = open("input.txt").readlines()
 
-    # Initialize state: {column_index: number_of_timelines}
-    # We start with 1 timeline at the 'S' position.
-    start_index = lines[0].find("S")
     current_timelines = defaultdict(int)
-    current_timelines[start_index] = 1
+    current_timelines[lines[0].find("S")] = 1
 
-    for line in lines[1:]:
-
-        if '^' not in line: continue
+    for line in lines:
+        if "^" not in line: continue
 
         next_timelines = defaultdict(int)
 
-        # Iterate through every position where we currently have active beams
-        for col_idx, count in current_timelines.items():
-
-            if line[col_idx] == '^':
-                # SPLIT: The count duplicates to both left and right timelines
-                # If 50 timelines hit this splitter, 50 go left AND 50 go right.
-                next_timelines[col_idx - 1] += count
-                next_timelines[col_idx + 1] += count
+        for idx, n_timelines in current_timelines.items():
+            if line[idx] == "^":
+                # Split the timelines
+                # Each new timeline carries along all the previous ones
+                next_timelines[idx+1] += n_timelines
+                next_timelines[idx-1] += n_timelines
             else:
-                # PASS: The count continues straight down
-                # If 50 timelines hit empty space, 50 continue to the same index.
-                next_timelines[col_idx] += count
+                # Pass the timeline along
+                next_timelines[idx] += n_timelines
 
-        # Move to the next row
         current_timelines = next_timelines
 
-    # The result is the total number of timelines active at the very end
-    print(f"Total timelines: {sum(current_timelines.values())}")
+    print(sum(current_timelines.values()))
 
 if __name__ == "__main__":
     num_timelines()
